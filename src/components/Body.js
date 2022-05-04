c2235118830a8a4a6acb6e1e29d3fba51191d625
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-05-05 01:44:31 +0530

    final commit final

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,7 +7,12 @@ import Panel4 from "./Panels/Panel4";
 import Panel5 from "./Panels/Panel5";
 import Panel6 from "./Panels/Panel6";
 
-import { fadeInLeft, fadeInRight, fadeInDown, fadeInUp } from "react-animations";
+import {
+  fadeInLeft,
+  fadeInRight,
+  fadeInDown,
+  fadeInUp,
+} from "react-animations";
 import Radium, { StyleRoot } from "radium";
 
 function Body(props) {
@@ -27,30 +32,30 @@ function Body(props) {
     fadeInUp: {
       animation: "x 1s",
       animationName: Radium.keyframes(fadeInUp, "fadeInUp"),
-    }
+    },
   };
   return (
     <StyleRoot>
       <div className="container-fluid parent-container">
         <div className="row">
-          <div className="col my-4" style={styles.fadeInLeft}>
+          <div className="col my-4 mx-4" style={styles.fadeInLeft}>
             <Panel1 mode={props.mode} />
           </div>
-          <div className="col my-4" style={styles.fadeInDown}>
+          <div className="col my-4 mx-4" style={styles.fadeInDown}>
             <Panel2 mode={props.mode} />
           </div>
-          <div className="col my-4" style={styles.fadeInRight}>
+          <div className="col my-4 mx-4" style={styles.fadeInRight}>
             <Panel3 mode={props.mode} />
           </div>
         </div>
         <div className="row">
-          <div className="col my-4" style={styles.fadeInLeft}>
+          <div className="col my-4 mx-4" style={styles.fadeInLeft}>
             <Panel4 mode={props.mode} />
           </div>
-          <div className="col my-4" style={styles.fadeInUp}>
+          <div className="col my-4 mx-4" style={styles.fadeInUp}>
             <Panel5 mode={props.mode} />
           </div>
-          <div className="col my-4" style={styles.fadeInRight}>
+          <div className="col my-4 mx-4" style={styles.fadeInRight}>
             <Panel6 mode={props.mode} />
           </div>
         </div>
